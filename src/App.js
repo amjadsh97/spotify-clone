4769d6d13784efcaee6bf15db2e1e41e5ee93eb7
Amjad Shadid <amjadshadid51@gmail.com>
2020-10-28 10:25:03 +0300

    end spotify

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Login from "./Login";
+import {getTokenFromUrl} from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import {useStateValue} from "./DataLayer";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
+
+    //const [token, setToken] = useState(null);
+
+    const [{user}, dispatch] = useStateValue();
+
+    useEffect(() => {
+        const hash = getTokenFromUrl();
+        window.location.hash = '';
+        const _token = hash.access_token;
+
+        if (_token) {
+            setToken(_token);
+            spotify.setAccessToken(_token);
+            spotify.getMe().then(user => {
+                console.log(user);
+                dispatch({
+                    type: 'SET_USER',
+                    user: user
+                })
+            });
+        }
+
+    }, []);
+
+    console.log(user, '😊\n');
+
     return (
         <div className="app">
-            <h1>spotify clone</h1>
+
+            {token ? <Player/> : <Login/>}
+
         </div>
     );
 }
